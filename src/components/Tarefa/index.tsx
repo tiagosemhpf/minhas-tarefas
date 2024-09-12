ac5ffd7d4e0685704b0120baa5490a63e1c60bfb
Tiago Pereira <tiagopereiralopesdossantos@gmail.com>
2024-09-12 13:29:57 -0300

    card de tarefa

---
+++ src/components/Tarefa/index.tsx
@@ -0,0 +1,16 @@
+import * as S from './styles'
+
+const Tarefa = () => (
+  <S.Card>
+    <S.Titulo>Nome da tarefa</S.Titulo>
+    <S.Tag>Importante</S.Tag>
+    <S.Tag>Pentende</S.Tag>
+    <S.Descricao />
+    <S.BarraAcoes>
+      <S.Botao>Editar</S.Botao>
+      <S.Botao>Remover</S.Botao>
+    </S.BarraAcoes>
+  </S.Card>
+)
+
+export default Tarefa
