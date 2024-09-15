7f29aa396c82d1faec906696e492bbbbda01ad11
Tiago Pereira <tiagopereiralopesdossantos@gmail.com>
2024-09-15 10:37:31 -0300

    Pesquise tarefas

---
+++ src/store/reducers/filtro.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import * as enums from '../../utils/enums/Tarefa'
+
+type FiltroStade = {
+  termo: string
+  criterio: 'prioridade' | 'status' | 'todas'
+  valor?: enums.Prioridade | enums.Status
+}
+
+const initialState: FiltroStade = {
+  termo: '',
+  criterio: 'todas'
+}
+
+const filtroSlice = createSlice({
+  name: 'filtro',
+  initialState,
+  reducers: {
+    alterarTermo: (state, action: PayloadAction<string>) => {
+      state.termo = action.payload
+    }
+  }
+})
+
+export const { alterarTermo } = filtroSlice.actions
+
+export default filtroSlice.reducer
