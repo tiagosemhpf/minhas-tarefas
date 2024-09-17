75ea5eca175b14f5a164d63b352ea5b49f58fec3
Tiago Pereira <tiagopereiralopesdossantos@gmail.com>
2024-09-17 13:28:13 -0300

    react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import BarraLateral from './containers/BarraLateral'
-import ListaDeTarefas from './containers/ListaDeTarefas'
-import EstiloGlobal, { Container } from './styles'
 import { Provider } from 'react-redux'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import EstiloGlobal, { Container } from './styles'
 import store from './store'
+import Home from './pages/home'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/novo',
+    element: <h1>novo</h1>
+  }
+])
 
 function App() {
   return (
     <Provider store={store}>
       <EstiloGlobal />
       <Container>
-        <BarraLateral />
-        <ListaDeTarefas />
+        <RouterProvider router={rotas} />
       </Container>
     </Provider>
   )
